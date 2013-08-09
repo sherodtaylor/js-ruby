886ef253cca767f416fb818d5822998caace2d15
Sherod Taylor <sherodtaylor@gmail.com>
2013-08-08 17:07:18 -0700

    Filter methods tests

--- tests/spec/RubyJS-Spec.js
+++ tests/spec/RubyJS-Spec.js
@@ -14,44 +14,57 @@ describe("Ruby Methods", function (){
     });
   });
 
-  describe("Map Method", function (){
-    it("maps each value in an Array and returns an Array of transformed values", function (){
-      var array = [1,2,3];
-      expect(Ruby.map( array, function ( n ){ return n * 2; })).toEqual([2,4,6]);
+  describe("Collection Methods", function (){
+    describe("Map Method", function (){
+      it("maps each value in an Array and returns an Array of transformed values", function (){
+        var array = [1,2,3];
+        expect(Ruby.map( array, function ( n ){ return n * 2; })).toEqual([2,4,6]);
+      });
+      it("maps each value in an Object and returns an Array of transformed values", function (){
+        var obj = { one: 1, two: 2, three: 3 };
+        expect(Ruby.map( obj, function ( n ){ return n * 2; })).toEqual([2,4,6]);
+      });
     });
-    it("maps each value in an Object and returns an Array of transformed values", function (){
-      var obj = { one: 1, two: 2, three: 3 };
-      expect(Ruby.map( obj, function ( n ){ return n * 2; })).toEqual([2,4,6]);
-    });
-  });
 
-  describe("Reduce Method", function (){
-    it("combines all the values of an Array", function (){
-      var array = [1,2,3];
-      expect(Ruby.reduce(array, function ( memo, n ){ return memo + n }, 0)).toEqual(6);
+    describe("Reduce Method", function (){
+      it("combines all the values of an Array", function (){
+        var array = [1,2,3];
+        expect(Ruby.reduce(array, function ( memo, n ){ return memo + n }, 0)).toEqual(6);
+      });
+      it("combies all values of an Object", function (){
+        var obj = { one: 1, two: 2 ,three: 3 };
+        expect(Ruby.reduce(obj, function ( memo, n ){ return memo + n }, 0)).toEqual(6);
+      });
     });
-    it("combies all values of an Object", function (){
-      var obj = { one: 1, two: 2 ,three: 3 };
-      expect(Ruby.reduce(obj, function ( memo, n ){ return memo + n }, 0)).toEqual(6);
-    });
-  });
 
-  describe("Find Method", function (){
-    it("looks through an Array and returns the first value that passes", function (){
-      var array = [1,2,3,4,5];
-      expect(Ruby.find(array, function ( num ){ return num % 2 === 0 } )).toEqual(2);
+    describe("Find Method", function (){
+      it("looks through an Array and returns the first value that passes", function (){
+        var array = [1,2,3,4,5];
+        expect(Ruby.find(array, function ( num ){ return num % 2 === 0 } )).toEqual(2);
+      });
+      it("looks through an Object and returns the first value that passes", function (){
+        var obj = { one:1, two:2, three:3, four:4, five:5 };
+        expect(Ruby.find(obj, function ( num ){ return num % 2 === 0 } )).toEqual(2);
+      });
+      it("Returns undefined if truth doesn't pass - Array", function (){
+        var array = [1,3,5];
+        expect(Ruby.find(array, function ( num ){ return num % 2 === 0 } )).toBeUndefined();
+      });
+      it("Returns undefined if truth doesn't pass - Object", function (){
+        var obj = { one:1, three:3, five:5 };
+        expect(Ruby.find(obj, function ( num ){ return num % 2 === 0 } )).toBeUndefined();
+      });
     });
-    it("looks through an Object and returns the first value that passes", function (){
-      var obj = { one:1, two:2, three:3, four:4, five:5 };
-      expect(Ruby.find(obj, function ( num ){ return num % 2 === 0 } )).toEqual(2);
-    });
-    it("Returns undefined if truth doesn't pass", function (){
-      var array = [1,3,5];
-      expect(Ruby.find(array, function ( num ){ return num % 2 === 0 } )).toBeUndefined();
-    });
-    it("Returns undefined if truth doesn't pass", function (){
-      var obj = { one:1, three:3, five:5 };
-      expect(Ruby.find(obj, function ( num ){ return num % 2 === 0 } )).toBeUndefined();
+
+    describe("Filter Method", function (){
+      it("looks through each value in an Array and returns an Array of values that pass truth test", function (){
+        var array = [1,2,3,4,5];
+        expect(Ruby.filter(array, function ( n ){ n % 2 === 0 }) ).toEqual([2,4]);
+      });
+      it("looks through each value in an Object and returns an Array of values that pass truth test", function (){
+        var obj = { one:1, two:2, three:3, four:4, five:5 };
+        expect(Ruby.filter(array, function ( n ){ n % 2 === 0 }) ).toEqual([2,4]);
+      });
     });
   });
 });

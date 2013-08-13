e97952c42cf6374673d174f7df5c694f193f64dc
Sherod Taylor <sherodtaylor@gmail.com>
2013-08-13 02:31:18 -0700

    Grunt automation added and working on travis-ci build

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: '<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    },
+    jasmine: {
+      src: '<%= pkg.name %>.js',
+      options: {
+        specs: 'tests/spec/RubyJS-Spec.js',
+        version: '1.3.1'
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task.
+  grunt.registerTask('default', ['jasmine','uglify']);
+
+  // // Travis CI task.
+};
